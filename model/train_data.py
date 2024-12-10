8b9a7f7210ed3a2d2f416bddf91553b7c1491c0e
Chessmen <sonlam7220@gmail.com>
2024-12-10 09:37:28 +0000

    nothing

--- model/train_data.py
+++ model/train_data.py
@@ -1,7 +1,14 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-from config.phishing_cfg import DatasetConfig
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from config.phishing_cfg import DatasetConfig, ModelConfig
+
+import pandas as pd
+import joblib
 
 
 class TrainingData():
